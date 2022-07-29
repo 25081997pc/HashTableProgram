c28332e66d57e997b64bf9469a0986744badd6c3
25081997pc <chavanpooja2508@gmail.com>
2022-07-29 19:52:25 +0530

    Using Hash Table

---
+++ FrequencyOfWords.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTable_Demo
+{
+    public class FrequencyOfWords
+    {
+        Hashtable MyMapNode = new Hashtable();
+
+        private String[] sentenceSplit;
+
+        public string[] splitString(string sentence)
+        {
+            return sentence.Split();
+        }
+
+        public void countWords(string[] sentenceSplit)
+        {
+            foreach(String str in sentenceSplit)
+            {
+                if(MyMapNode.ContainsKey(str))
+                {
+                    MyMapNode[str] = (int)MyMapNode[str] + 1;
+                }
+                else
+                {
+                    MyMapNode.Add(str, 1);
+                }
+            }
+            foreach(DictionaryEntry item in MyMapNode)
+            {
+                Console.WriteLine(item.Key + " " + item.Value);
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,6 +8,23 @@ namespace HashTable_Demo
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to The Hash Table Program");
+            Console.WriteLine("Hash Table Demo");
+            MyMapNode<string, string> hash = new MyMapNode<string, string>(5);
+            hash.Add("0", "To");
+            hash.Add("1", "be");
+            hash.Add("2", "or");
+            hash.Add("3", "not");
+            hash.Add("4", "to");
+            hash.Add("5", "be");
+            string hash5 = hash.Get("5");
+            Console.WriteLine("5th index value: " + hash5);
+            string hash2 = hash.Get("2");
+            Console.WriteLine("2th index value: " + hash2);
+
+            FrequencyOfWords frequency = new FrequencyOfWords();
+            string s = "to be or not to be";
+            string[] sentenceSplit = frequency.splitString(s);
+            frequency.countWords(sentenceSplit);
         }
     }
 }
