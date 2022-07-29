af5019e1da8fb4cdbc8d5033b5fe4891dec94ac4
25081997pc <chavanpooja2508@gmail.com>
2022-07-29 19:58:13 +0530

    Counting Frequency of Words

--- Program.cs
+++ Program.cs
@@ -9,20 +9,9 @@ namespace HashTable_Demo
         {
             Console.WriteLine("Welcome to The Hash Table Program");
             Console.WriteLine("Hash Table Demo");
-            MyMapNode<string, string> hash = new MyMapNode<string, string>(5);
-            hash.Add("0", "To");
-            hash.Add("1", "be");
-            hash.Add("2", "or");
-            hash.Add("3", "not");
-            hash.Add("4", "to");
-            hash.Add("5", "be");
-            string hash5 = hash.Get("5");
-            Console.WriteLine("5th index value: " + hash5);
-            string hash2 = hash.Get("2");
-            Console.WriteLine("2th index value: " + hash2);
-
             FrequencyOfWords frequency = new FrequencyOfWords();
-            string s = "to be or not to be";
+            Console.WriteLine("Frequency of Words : ");
+            string s = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
             string[] sentenceSplit = frequency.splitString(s);
             frequency.countWords(sentenceSplit);
         }
